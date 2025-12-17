9c7ba8e92a068ecb6c0f2bea28650c3d8b15c069
Henk van de Ridder <henk.vande.ridder@gmail.com>
2025-12-17 12:09:04 +0100

    CORS handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Http\Middleware\HandleCors;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->prependToGroup('api', HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
